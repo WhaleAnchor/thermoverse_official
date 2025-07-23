de5a9ab18ebcfd05804d415506a0e1878ca25af6
Andrew Choi <andrewsungwoochoi@gmail.com>
2025-07-23 19:36:58 -0400

    added google analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,25 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-7F4XZJJM06"
+        />
+        <script
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-7F4XZJJM06');
+            `,
+          }}
+        />
+        {/* any other <meta> or <link> tags */}
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
